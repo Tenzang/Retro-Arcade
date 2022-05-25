afc1092de089136fbf214295e1e795865a25e168
kristabel-wong <kristabel.wong@hotmail.com>
2022-05-25 23:47:02 +1000

    added controls, speed and game over function - still needs refining and debugging

--- retro-arcade/src/components/snake/Controls.js
+++ retro-arcade/src/components/snake/Controls.js
@@ -2,13 +2,12 @@ import React from "react";
 
 const Controls = ({setPlay, direction, setDirection}) => {
 
-    function handlePlay(e, val) {
+    function handlePlay(e, val) { // play or pause
         e.preventDefault();
         (val ? setPlay(true) : setPlay(false))
-        console.log(val)
     }
 
-    function handleDirection(e, val) {
+    function handleDirection(e, val) { // set direction of snake 
         e.preventDefault();
         switch(val) {
             case 'ArrowLeft':
@@ -31,16 +30,16 @@ const Controls = ({setPlay, direction, setDirection}) => {
 
 
     return (
-        <div>
-            <div>
-                <button onClick={(e) => handlePlay(e, true)}>Play</button>
-                <button onClick={(e) => handlePlay(e, false)}>Pause</button>
+        <div className="controls">
+            <div className="play-pause">
+                <button className="play" onClick={(e) => handlePlay(e, true)}>Play</button>
+                <button className="pause" onClick={(e) => handlePlay(e, false)}>Pause</button>
             </div>
-            <div>
-                <button onClick={(e) => handleDirection(e, 'ArrowUp')}>Up</button>
-                <button onClick={(e) => handleDirection(e, 'ArrowDown')}>Down</button>
-                <button onClick={(e) => handleDirection(e, 'ArrowLeft')}>Left</button>
-                <button onClick={(e) => handleDirection(e, 'ArrowRight')}>Right</button>
+            <div className="controls-direction">
+                <button className="left" onClick={(e) => handleDirection(e, 'ArrowLeft')}>Left</button>
+                <button className="up" onClick={(e) => handleDirection(e, 'ArrowUp')}>Up</button>
+                <button className="down" onClick={(e) => handleDirection(e, 'ArrowDown')}>Down</button>
+                <button className="right" onClick={(e) => handleDirection(e, 'ArrowRight')}>Right</button>
             </div>
         </div>
     )
--- retro-arcade/src/components/snake/SnakeBoard.js
+++ retro-arcade/src/components/snake/SnakeBoard.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
-import './Snake.css';
+import './Snake.scss';
 import Controls from './Controls';
+import _ from 'lodash';
 
 const SnakeBoard = () => {
 
     // grid 20 by 20 
     const width = 20;
     const height = 20;
-    const delay = 300;
+    const instruction = '[ Press "Space" to start ]'
+    // const initialSnake = [{x:0, y:0}]
 
     const initialRows = [];
     for (let i = 0; i < 20; i++) {
@@ -27,11 +29,14 @@ const SnakeBoard = () => {
     }
 
     const [rows, setRows] = useState(initialRows);
-    const [snake, setSnake] = useState([{x:0, y:0}]); // our initial position for the snake is x:0, y:0
+    const [initialSnake] = useState([{x:0, y:0}]);
+    const [snake, setSnake] = useState(_.cloneDeep(initialSnake)); // our initial position for the snake is x:0, y:0
     const [direction, setDirection] = useState('right'); // Figure out why this state is not updating
     const [food, setFood] = useState(randomPosition);
-    const [play, setPlay] = useState(true);
-
+    const [play, setPlay] = useState(false);
+    const [visibility, setVisibility] = useState('visible')
+    const [message, setMessage] = useState('Start')
+    const [delay, setDelay] = useState(300)
 
     //  console.log('snake', snake.length)
 
@@ -44,7 +49,7 @@ const SnakeBoard = () => {
                         case 'snake':
                             return (<div key={String(x) + String(y)} className="snake"></div>)
                         case 'food':
-                            return (<div key={String(x) + String(y)}  className="food"></div>)                       
+                            return (<div key={String(x) + String(y)}  className="food"><div className='egg'></div></div>)                       
                         }
                     })
                 }
@@ -84,12 +89,14 @@ const SnakeBoard = () => {
                 newSnake.push(cell);
             })
 
-            if (snake[0].x === food.x && snake[0].y === food.y) {
+            if (snake[0].x === food.x && snake[0].y === food.y) { // when the snake eats food
                 setFood(randomPosition);
+                increaseSpeed()
             } else if (snake.length > 4) { // 
                 for (let i = 4; i < snake.length; i++) {
                     if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
-                        console.log('collision')
+                        console.log('collision & game over')
+                        gameOver()
                     } 
                 }
                 newSnake.pop();
@@ -131,19 +138,15 @@ const SnakeBoard = () => {
         switch(key) {
             case 'ArrowLeft':
                 (direction === 'right') ? setDirection('right') : setDirection('left')
-                console.log('ArrowLeft', direction); // async 
                 break;
             case 'ArrowRight':
                 (direction === 'left') ? setDirection('left') : setDirection('right')
-                console.log('ArrowRight', direction);
                 break;
             case 'ArrowUp':
                 (direction === 'down') ? setDirection('down') : setDirection('up')
-                console.log('ArrowUp', direction);
                 break;
             case 'ArrowDown':
                (direction === 'up') ? setDirection('up') : setDirection('down')
-                console.log('ArrowDown', direction);
                 break;
             default:
                 break;
@@ -157,14 +160,54 @@ const SnakeBoard = () => {
         return () => {
             document.removeEventListener("keyup", changeDirectionWithKeys);
         }
-      
+        
     },[direction])
+    
 
+    const start = (e) => { //start game with a space bar - remove modal & event listener
+        console.log('start', e.code)
+        switch(e.code) {
+            case 'Space':
+                setPlay(true)
+                setVisibility('hidden')
+                document.removeEventListener("keyup", start);
+                break;
+        }
+    }
+
+    const gameOver = () => {
+        setMessage('Game Over')
+        setVisibility('visible')
+        setSnake(_.cloneDeep(initialSnake))
+        setPlay(false)
+
+    }
+
+    useEffect(() => {
+        document.addEventListener("keyup", start);
+        return () => {
+            document.removeEventListener("keyup", start);
+        }
+    },[gameOver])
+
+    const increaseSpeed = () => {
+        let level = snake.length-1
+        if (level === 10 || level === 20 || level === 25 || level === 30 || level === 40) {
+            let speed = delay - 50 // times: 250, 200, 150, 100, 50
+            console.log('speed',speed)
+            setDelay(speed)
+        }
+    }
 
     return (
         <div className='Snakeboard'>
             <h1>Snake</h1>
             <div className='snakeGame'>{displayRows}</div>
+            <div className='message' style={{visibility: visibility}}>
+                <div className='messageText'>{message}</div>
+                <div className='instruction' style={{visibility: visibility}}>{instruction}</div>
+                </div>
+            <div>Points: {snake.length-1}</div>
             <Controls setPlay={setPlay} direction={direction} setDirection={setDirection} />
         </div>
     )
